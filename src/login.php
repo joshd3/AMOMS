351e3d83e752c7a65f7e8ab00d7ed521dc94b25a
Joshua D <joshd3@users.noreply.github.com>
2017-07-12 22:12:37 -0400

    Create login.php

---
+++ src/login.php
@@ -0,0 +1,12 @@
+<div id="mainContent">
+  <form id="loginForm" method="post" action="index.php">
+    <p>UserName / Nombre de Usario
+       <br /><input type="text" name="userId" id="userId" />
+       <br />Password / Contraseña
+       <br /><input type="password" name="pwd" id="pwd" />
+       <br /><br /><input name="english" type="submit" value="Login in English" />&nbsp;&nbsp;&nbsp;<input name="spanish"type="submit" value="Ingresar en español" />
+       <br /><br /><input name="forgotUserName" type="submit" value="Forgot UserName / Nombre de usuario olvidado" />
+		&nbsp;&nbsp;&nbsp;<input name="forgotPassword"type="submit" value="Forgot Password / Se te olvidó tu contraseña" />
+	</p>
+  </form>
+</div>
